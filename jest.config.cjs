b76669b432a9f68ed1b2da720c4fbdced13ee192
thkiti <40308235+thkiti@users.noreply.github.com>
2026-06-10 04:23:24 +0700

    fix vercel build exclude test script from next typecheck

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
+  },
   testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.tsx"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup.ts"],
   moduleNameMapper: {
